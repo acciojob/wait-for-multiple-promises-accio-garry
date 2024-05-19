b0ad160e4c48394a5de8459700f7d3f239f7892f
accio-garry <168645773+accio-garry@users.noreply.github.com>
2024-05-19 22:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -40,11 +40,11 @@ Promise.all([p1, p2, p3]).then((times) => {
     document.getElementById("promise-1").innerHTML = times[0] / 1000; // divide by 1000 to convert ms to s
     document.getElementById("promise-2").innerHTML = times[1] / 1000; 
     document.getElementById("promise-3").innerHTML = times[2] / 1000; 
-
+     let loadingRow = document.getElementById("loading");
+     loadingRow.parentNode.removeChild(loadingRow);
     let endTime = Date.now();
     let totalTime = endTime - startTime; // calculate total time
     document.getElementById("total").innerHTML = totalTime / 1000; // display total time
-	 let loadingRow = document.getElementById("loading");
-     loadingRow.parentNode.removeChild(loadingRow);
+	 
 });
-
+ 
